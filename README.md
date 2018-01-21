c2d3366815b44030887bc417b058d0cadbf4a242
Darwin <dhdo@ncsu.edu>
2018-01-21 15:27:26 -0800

    finish part 7

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## SHOPPING CART with NodeJS/ Express/ MongoDB
 
-- Link <a href="https://www.youtube.com/watch?v=GHNLWHGCBEc">NodeJS / Express / MongoDB - Build a Shopping Cart </a>
+- Part 6 <a href="https://www.youtube.com/watch?v=waAqEjjssxU">Sign Up View, CSRF Protection & User Model</a>
+- Part 7 <a href="https://www.youtube.com/watch?v=GHNLWHGCBEc">Sign Up with Passport</a>
--- app.js
+++ app.js
@@ -8,11 +8,13 @@ var expressHbs = require("express-handlebars");
 var index = require('./routes/index');
 var mongoose = require("mongoose");
 var session = require("express-session")
+var passport  = require("passport");
+var flash = require("connect-flash");
 var app = express();
 
 
 mongoose.connect("mongodb://localhost:27017/shopping")
-
+require('./config/passport');
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +28,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: "mysupersecret", resave: false, saveUninitialized: false}))
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
