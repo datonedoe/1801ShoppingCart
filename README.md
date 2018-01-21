1a77beaecaf3b290ac954a740f0e1109ef8e9c76
Darwin <dhdo@ncsu.edu>
2018-01-21 13:10:02 -0800

    add README, finished part 6

---
+++ README.md
@@ -0,0 +1,3 @@
+## SHOPPING CART with NodeJS/ Express/ MongoDB
+
+- Link <a href="https://www.youtube.com/watch?v=GHNLWHGCBEc">NodeJS / Express / MongoDB - Build a Shopping Cart </a>
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var expressHbs = require("express-handlebars");
 var index = require('./routes/index');
 var mongoose = require("mongoose");
-
+var session = require("express-session")
 var app = express();
 
 
@@ -25,6 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: "mysupersecret", resave: false, saveUninitialized: false}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
